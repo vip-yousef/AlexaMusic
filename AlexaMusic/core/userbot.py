0b668a3d0abc31a63f34bddf6baa2d43b6ce29cc
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-01 23:40:17 +0300

    Update userbot.py

--- AlexaMusic/core/userbot.py
+++ AlexaMusic/core/userbot.py
@@ -65,9 +65,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Alexa_Help")
-                await self.one.join_chat("TheTeamAlexa")
-                await self.one.join_chat("Alexa_BotUpdates")
+                await self.one.join_chat("cecrr")
+                await self.one.join_chat("kkc8c")
+                await self.one.join_chat("def_Zoka")
             except:
                 pass
             assistants.append(1)
@@ -121,9 +121,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Alexa_Help")
-                await self.three.join_chat("TheTeamAlexa")
-                await self.three.join_chat("Alexa_BotUpdates")
+                await self.three.join_chat("cecrr")
+                await self.three.join_chat("kkc8c")
+                await self.three.join_chat("def_Zoka")
             except:
                 pass
             assistants.append(3)
@@ -149,9 +149,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Alexa_Help")
-                await self.four.join_chat("TheTeamAlexa")
-                await self.four.join_chat("Alexa_BotUpdates")
+                await self.four.join_chat("cecrr")
+                await self.four.join_chat("kkc8c")
+                await self.four.join_chat("def_Zoka")
             except:
                 pass
             assistants.append(4)
@@ -177,9 +177,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Alexa_Help")
-                await self.five.join_chat("TheTeamAlexa")
-                await self.five.join_chat("Alexa_BotUpdates")
+                await self.five.join_chat("cecrr")
+                await self.five.join_chat("kkc8c")
+                await self.five.join_chat("def_Zoka")
             except:
                 pass
             assistants.append(5)
