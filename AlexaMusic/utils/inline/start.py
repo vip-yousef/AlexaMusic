d9db26fe563b264624136f878263804ae0deda95
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2024-06-01 22:44:43 +0300

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -1,17 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
-from typing import Union
-
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
@@ -75,14 +61,19 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ]
     )
-    if OWNER:
+    if GITHUB_REPO and OWNER:
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER"),
             ]
+        )
+    else:
+        if GITHUB_REPO:
             buttons.append(
                 [
-                
+                    InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER"),
+                ]
             )
         if OWNER:
             buttons.append(
